190489cb4c882a91fc63117712d95f759ac03dd8
Guido De Rosa <job@guidoderosa.net>
2009-11-18 19:12:28 +0000

    patch escape_json_string to make it more UTF-8-safe

--- lib/json_printer.rb
+++ lib/json_printer.rb
@@ -133,13 +133,18 @@ class JsonPrinter
   # String#to_json extracted from ActiveSupport, using interpolation for speed.
   #
   def escape_json_string(str)
-    "\"#{
-    str.gsub(/[\010\f\n\r\t"\\><&]/) { |s| ESCAPED_CHARS[s] }.
-        gsub(/([\xC0-\xDF][\x80-\xBF]|
-               [\xE0-\xEF][\x80-\xBF]{2}|
-               [\xF0-\xF7][\x80-\xBF]{3})+/nx) do |s|
-          s.unpack("U*").pack("n*").unpack("H*")[0].gsub(/.{4}/, '\\\\u\&')
-        end
-    }\""
+    begin
+      "\"#{
+      str.gsub(/[\010\f\n\r\t"\\><&]/) { |s| ESCAPED_CHARS[s] }.
+          gsub(/([\xC0-\xDF][\x80-\xBF]|
+                 [\xE0-\xEF][\x80-\xBF]{2}|
+                 [\xF0-\xF7][\x80-\xBF]{3})+/nx) do |s|
+            s.unpack("U*").pack("n*").unpack("H*")[0].gsub(/.{4}/, '\\\\u\&')
+          end
+      }\""
+    rescue Encoding::CompatibilityError
+      rawbytes = str.dup.force_encoding 'ASCII-8BIT'
+      escape_json_string(rawbytes)
+    end
   end
 end
